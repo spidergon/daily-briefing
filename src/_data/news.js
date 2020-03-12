3c1fc8b96606b76257c03e1e9c7427a1587751e3
Christopher Servius <spidergon@gmail.com>
2020-03-12 00:40:10 -0300

    descriptive env var

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = async () => {
   try {
-    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=fr&category=business&apiKey=${process.env.API_KEY}&pageSize=5`);
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=fr&category=business&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
     return response.data
   } catch (error) {
     console.error(error);
