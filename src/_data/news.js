66c4a4dfd33d9af3b704cacebccfc406a24940cb
Christopher Servius <spidergon@gmail.com>
2020-03-13 11:52:31 -0300

    rewrites

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,11 +1,23 @@
-const axios = require("axios")
 require('dotenv').config()
 
-module.exports = async () => {
+const axios = require("axios")
+const countries = require('./countries.json')
+
+async function getNews (country) {
   try {
-    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=fr&category=business&apiKey=${process.env.NEWS_API_KEY}&pageSize=10`);
-    return response.data
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&category=business&apiKey=${process.env.NEWS_API_KEY}&pageSize=10`);
+    return {
+      country,
+      articles: response.data.articles
+    }
   } catch (error) {
     console.error(error);
   }
+}
+
+module.exports = async () => {
+  const newsPromises = countries.map(getNews)
+  return Promise.all(newsPromises).then(newsObjects => {
+    return [].concat.apply([], newsObjects)
+  })
 }
