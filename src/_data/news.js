cf478ac10abfa6ee7ab8f85fd6d10b6e90bb7aec
Christopher Servius <spidergon@gmail.com>
2020-03-13 12:26:13 -0300

    remove lots of countries

--- src/_data/news.js
+++ src/_data/news.js
@@ -11,13 +11,31 @@ async function getNews (country) {
       articles: response.data.articles
     }
   } catch (error) {
-    console.error(error);
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+
+    return {country, articles: []}
   }
 }
 
 module.exports = async () => {
   const newsPromises = countries.map(getNews)
   return Promise.all(newsPromises).then(newsObjects => {
+    console.log(newsObjects)
     return [].concat.apply([], newsObjects)
   })
 }
