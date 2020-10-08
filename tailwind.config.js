175d528bf76d39476d5f8b9dd296f87abf83efed
Chris <spidergon@gmail.com>
2020-10-08 20:56:19 -0300

    updates and corrections

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,5 +20,9 @@ module.exports = {
     extend: {}
   },
   variants: {},
-  plugins: []
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  }
 }
